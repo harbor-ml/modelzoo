49d2d7baab1d32dcb124bab443f277bec6bf9f62
Rehan S. Durrani <rdurrani@berkeley.edu>
2019-07-02 18:53:40 -0700

    Fix port from 8080 -> 9090

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -8,7 +8,7 @@ import { Models } from "./Components/Models";
 
 const { Title } = Typography;
 
-const client = new ModelClient("34.207.114.194:9090", null, null);
+const client = new ModelClient("34.207.114.194:8080", null, null);
 export const ClientContext = React.createContext(client);
 
 const App: FC = () => {
