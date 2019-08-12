39ac15e6b2eed7af6677c6148dd85b15f852835e
Rehan S. Durrani <rdurrani@berkeley.edu>
2019-08-12 14:15:24 -0700

    Fix init

--- modelzoo/__init__.py
+++ modelzoo/__init__.py
@@ -1,1 +1,7 @@
 name = "modelzoo"
+
+from query import create_image_seg_req, create_text_gen_req, create_vision_gen_req, query_segmentation, query_text, query_vision
+from utils import img_file_to_uri, img_to_uri, uri_to_img, uri_to_img_file, ImgLike
+
+__all__ = ["create_image_seg_req", "create_text_gen_req", "create_vision_gen_req", "query_segmentation", "query_text", "query_vision",
+"img_file_to_uri", "img_to_uri", "uri_to_img", "uri_to_img_file", "ImgLike"]
