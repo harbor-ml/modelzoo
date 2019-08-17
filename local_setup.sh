5d5e4554a6701ec90b511fd189ab28a35afac20e
Rehan S. Durrani <rdurrani@berkeley.edu>
2019-08-16 18:32:45 -0700

    Add go dependencies

--- local_setup.sh
+++ local_setup.sh
@@ -1,3 +1,7 @@
 mkdir $GOPATH/src/modelzoo/go/dbtypes
 cp /go/sql_dbyptes.go $GOPATH/src/modelzoo/go/dbtypes/
+go get -u github.com/jinzhu/gorm
+go get github.com/google/uuid
+go get -u github.com/mattn/go-sqlite3
 go run go/setup_db.go
+echo "Just run docker-compose -f docker-compose.local.yml up to run ModelZoo.Live locally."
