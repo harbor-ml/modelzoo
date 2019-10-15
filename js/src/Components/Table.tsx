6f956abfcc002912b0fdd093da5511ec1ca089ce
Simon Mo <xmo@berkeley.edu>
2019-10-14 19:56:14 -0700

    Add table.tsx

---
+++ js/src/Components/Table.tsx
@@ -0,0 +1,37 @@
+import { Table } from "antd";
+import { ColumnProps } from "antd/lib/table";
+import _ from "lodash";
+import { Table as pb_Table } from "protos/services_pb";
+import React, { FC } from "react";
+
+interface TableOutputProps {
+  tableProto: pb_Table;
+}
+export const TableOutput: FC<TableOutputProps> = props => {
+  let { tableProto } = props;
+
+  let antColumns: Array<
+    ColumnProps<string>
+  > = tableProto.getColumnNamesList().map((val, idx, arr) => {
+    return { key: idx, title: val.toUpperCase(), dataIndex: val };
+  });
+
+  let dataSource = tableProto.toObject().tableMap.map((val, idx, arr) => {
+    console.log(val);
+    let rowName = val[0];
+    let rowData = val[1];
+    let [rowKeys, rowVals] = _.zip(...rowData.columnToValueMap)!;
+    let flattendObject = _.zipObject(_.compact(rowKeys), _.compact(rowVals));
+    flattendObject.key = rowName;
+    return flattendObject;
+  });
+
+  return (
+    <Table
+      style={{ background: "white" }}
+      columns={antColumns}
+      dataSource={dataSource as any}
+      pagination={{ pageSize: 10 }}
+    ></Table>
+  );
+};
